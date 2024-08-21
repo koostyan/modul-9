2e3e60d7003f1ed00f4b790bcc4d30afc5f1d416
koostyan <165835474+koostyan@users.noreply.github.com>
2024-08-21 19:53:24 +0300

    Add files via upload

---
+++ funkzii na letu.py
@@ -0,0 +1,41 @@
+# Lambda-функция:
+
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+result = list(map(lambda x, y: x == y, first, second))
+print(result)
+print()
+
+
+# Замыкание:
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'a', encoding='utf-8') as f:
+            for data in data_set:
+                f.write(str(data) + '\n')
+
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+print()
+
+# Метод __call__:
+
+import random
+
+
+class MysticBall:
+    def __init__(self, words):
+        self.words = words
+
+    def __call__(self):
+        return random.choice(self.words)
+
+
+first_ball = MysticBall('Да', 'Нет', 'Возможно', 'Возможно нет', 'Возможно да')
+print(first_ball())
+print(first_ball())
+print(first_ball())
